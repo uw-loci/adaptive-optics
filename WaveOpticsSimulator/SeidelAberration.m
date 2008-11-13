6d13c248a03be3a8ca4476318916c7d21a11879f
Gunnsteinn Hall <gunnsteinn.hall@gmail.com>
2008-11-13 21:42:58 +0000

    Fixed Seidel abb

--- WaveOpticsSimulator/SeidelAberration.m
+++ WaveOpticsSimulator/SeidelAberration.m
@@ -1,14 +1,15 @@
-%function [FO] = SeidelAberration(FI, deg, lambda) 
+%function [FO] = SeidelAberration(FI, deg, coeff, lambda) 
 %Input field FI is multiplied by the phase profile of the
 %aberration, producing FO as the output.
 %Lambda is the wavelength used.
 %Deg is the degree of the aberration, 4 for SA.
-function [FO] = SeidelAberration(FI, deg, lambda) 
+%Coeff is the coefficient of the aberration.
+function [FO] = SeidelAberration(FI, deg, coeff, lambda) 
 
 rho = sqrt((FI.SY).^2 + (FI.SX).^2);
 %rhoScaled = rho/(0.5*FI.opticalWidth);
 %theta = atan(SX./(SY+1e-12));
-phi = -rho.^(deg);
+phi = -coeff*rho.^(deg);
 
 k=2*pi/lambda;
 %Output
@@ -19,5 +20,6 @@ FO.opticalWidth=FI.opticalWidth;
 FO.E = FI.E .* exp(i*k*phi);
 
 FO.opName='SeidelAberration';
-FO.opParam=sprintf('deg=%d, lambda=%dnm', deg, lambda*1e9);
+FO.opParam=sprintf('deg=%d, coeff=%1.2f wl, lambda=%dnm', ...
+    deg, coeff/lambda, lambda*1e9);
 
