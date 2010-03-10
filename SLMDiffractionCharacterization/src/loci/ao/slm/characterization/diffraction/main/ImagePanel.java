f572062d86472b5b63a30088352f00d2d120432c
Gunnsteinn Hall <gunnsteinn.hall@gmail.com>
2010-03-10 18:04:50 +0000

    Updates SLM Diffraction Calibration GUI.

--- SLMDiffractionCharacterization/src/loci/ao/slm/characterization/diffraction/main/ImagePanel.java
+++ SLMDiffractionCharacterization/src/loci/ao/slm/characterization/diffraction/main/ImagePanel.java
@@ -12,13 +12,18 @@
 package loci.ao.slm.characterization.diffraction.main;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Image;
 import java.awt.Point;
-import java.awt.Toolkit;
+import java.awt.Rectangle;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.Observable;
+import java.util.Observer;
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 /**
@@ -26,37 +31,145 @@ import javax.swing.JPanel;
  */
 class ImagePanel extends JPanel implements MouseListener {
     /**
-     * The Image object to be displayed in the panel.
+     * The coordinates of the upper-left corner of the ROI.
      */
-    private Image image;
     private int x1, y1;
+
+    /**
+     * The coordinates of the lower-right corner of the ROI.
+     */
     private int x2, y2;
+
+    /**
+     * Helps with the state of ROI definition - associated with mouse clicks.
+     */
     private boolean isFirstPoint;
 
-    public ImagePanel() {
-        addMouseListener(this);
-        x1 = y1 = -1;
-        x2 = y2 = -1;
-        isFirstPoint = true;
-        image =
-                Toolkit.getDefaultToolkit().getImage(
-                "C:/Users/Public/Pictures/Sample Pictures/Tulips.jpg");
-        //setSize(image.getWidth(this), image.getHeight(this));
-    }
     /**
-     * Set the image to be displayed in the panel.
-     *
-     * @param image The image to be displayed.
+     * Contains the image to be displayed.
      */
-    public void setImage(Image image) {
-        this.image = image;
+    private BufferedImage img;
+
+    /**
+     * The maximum width of the Image Panel (in pixels).
+     */
+    private final int maxWidth = 800;
+
+    /**
+     * The maximum height of the Image Panel (in pixels).
+     */
+    private final int maxHeight = 600;
+
+    /**
+     * A notifier object used to inform observers when a change has been
+     * made, e.g. when a ROI has been defined.
+     */
+    private ImagePanelNotifier notifier;
+
+    /**
+     * Defines whether or not to use the ROI features.
+     */
+    private boolean enableROI;
+
+    /**
+     * Constructs the ImagePanel object.  Initializes and sets up the image.
+     */
+    public ImagePanel(boolean enableROI) {
+        this.enableROI = enableROI;
+        if (enableROI) {
+            addMouseListener(this);
+            x1 = y1 = -1;
+            x2 = y2 = -1;
+            isFirstPoint = true;
+            notifier = new ImagePanelNotifier();
+        }
+
+        // Load the image.
+
+        try {
+           img = ImageIO.read(new File("C:/Users/Public/Pictures/Sample Pictures/Tulips.jpg"));
+        } catch (IOException e) {
+        }
+
+        setPreferredSize(getPreferredSize());
     }
 
+    /**
+     * Add an observer to monitor the object.
+     * 
+     * @param obs The observer to be added.
+     */
+    public void addObserver(Observer obs) {
+        notifier.addObserver(obs);
+    }
+
+    /**
+     * Delete an observer.
+     *
+     * @param obs The observer to be deleted.
+     */
+    public void deleteObserver(Observer obs) {
+        notifier.deleteObserver(obs);
+    }
+
+    /**
+     * Overrides the default get preferred size function.
+     * Returns the size of the image that was loaded, up to a certain
+     * maximum width and height.  If not specified then returns a
+     * default value of max width, max height.
+     *
+     * @return The dimension of the image panel.
+     */
+    @Override
+    public Dimension getPreferredSize() {
+        if (img == null) {
+             return new Dimension(maxWidth, maxHeight);
+        } else {
+            int width = img.getWidth(null);
+            int height = img.getHeight(null);
+            if (width > maxWidth)
+                width = maxWidth;
+            if (height > maxHeight)
+                height = maxHeight;
+
+           return new Dimension(width, height);
+       }
+    }
+
+    /**
+     * Specifies the ROI (region of interest).
+     *
+     * @param x1 X-coordinate of the upper-left corner.
+     * @param y1 Y-coordinate of the upper-left corner.
+     * @param x2 X-coordinate of the lower-right corner.
+     * @param y2 Y-coordinate of the lower-right corner.
+     */
     public void setRectangle(int x1, int y1, int x2, int y2) {
         this.x1 = x1;
         this.y1 = y1;
         this.x2 = x2;
         this.y2 = y2;
+    }
+
+    /**
+     * Returns the ROI.
+     *
+     * @return The ROI (region of interest).
+     */
+    public Rectangle getROI() {
+        return new Rectangle(x1, y1, x2 - x1, y2 - y1);
+    }
+
+    /**
+     * Returns true if the ROI has been defined, false otherwise.
+     * 
+     * @return True if the ROI has been defined, false otherwise.
+     */
+    public boolean isROIDefined() {
+        if (x1 < 0 || x2 < 0 || y1 < 0 || y2 < 0)
+            return false;
+        else
+            return true;
     }
 
     /**
@@ -66,20 +179,24 @@ class ImagePanel extends JPanel implements MouseListener {
      */
     @Override
     public void paintComponent(Graphics g) {
-        //System.out.println("Paint Runs on: " + Thread.currentThread().getName());
         super.paintComponent(g);
-        g.drawImage(image, 0, 0, this);
+        g.drawImage(img, 0, 0, null);
         if (x1 >= 0 && x2 >= 0 && y1 >= 0 && y2 >= 0) {
+            g.setColor(Color.RED);
             g.drawRect(x1, y1, x2 - x1, y2 - y1);
         }
     }
 
-    public void mouseClicked(MouseEvent e) {
+    /**
+     * Acts when the mouse is clicked within the ImagePanel.  Used to specify
+     * the ROI.
+     *
+     * @param e The event description object.
+     */
+    public void mousePressed(MouseEvent e) {
         Point cursor = e.getLocationOnScreen();
 
-        System.out.println("Location: " + cursor.toString());
         if (isFirstPoint) {
-
             x1 = cursor.x - getLocationOnScreen().x;
             y1 = cursor.y - getLocationOnScreen().y;
             isFirstPoint = false;
@@ -87,13 +204,28 @@ class ImagePanel extends JPanel implements MouseListener {
             x2 = cursor.x - getLocationOnScreen().x;
             y2 = cursor.y - getLocationOnScreen().y;
             isFirstPoint = true;
-            System.out.println("Repainting rectangle");
             repaint();
+            notifier.notifyChange(this);
         }
     }
 
-    public void mousePressed(MouseEvent e) {}
+    /*
+     * Unsupported events.
+     */
+    public void mouseClicked(MouseEvent e) {}
     public void mouseReleased(MouseEvent e) {}
     public void mouseEntered(MouseEvent e) {}
     public void mouseExited(MouseEvent e) {}
+
+
+    /**
+     * The ImagePanelNotifier class is intended only as a means for the
+     * ImagePanel class to notify observers about changes.
+     */
+    class ImagePanelNotifier extends Observable {
+        protected void notifyChange(ImagePanel ip) {
+            setChanged();
+            notifyObservers(ip);
+        }
+    }
 }
--- SLMDiffractionCharacterization/src/loci/ao/slm/characterization/diffraction/main/Main.java
+++ SLMDiffractionCharacterization/src/loci/ao/slm/characterization/diffraction/main/Main.java
@@ -14,21 +14,19 @@ package loci.ao.slm.characterization.diffraction.main;
 import com.jgoodies.forms.builder.PanelBuilder;
 import com.jgoodies.forms.layout.CellConstraints;
 import com.jgoodies.forms.layout.FormLayout;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.beans.PropertyChangeListener;
 import java.io.File;
 import java.util.Observable;
 import java.util.Observer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.Action;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JSpinner;
 import javax.swing.JTextField;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
@@ -41,28 +39,74 @@ import loci.hardware.camera.CCDCamera;
  * The Main class handles controlling the flow of control and setting up
  * the experiment as well as displaying the user interface.
  */
-public class Main extends JFrame {
-    JTextField upperLeftX;
-    JTextField upperLeftY;
-    JTextField lowerRightX;
-    JTextField lowerRightY;
-    ImagePanel imagePanel;
-    JFileChooser folderChooser;
-    JTextField outPathEdit;
-    JButton browseButton;
+public class Main extends JFrame implements Observer {
+    /**
+     * Textfield that specifies the x coordinate of the upper left corner of
+     * the ROI.
+     */
+    private JTextField upperLeftX;
 
+    /**
+     * Textfield that specifies the y coordinate of the upper left corner of
+     * the ROI.
+     */
+    private JTextField upperLeftY;
 
+    /**
+     * Textfield that specifies the x coordinate of the lower right corner of
+     * the ROI.
+     */
+    private JTextField lowerRightX;
+
+    /**
+     * Textfield that specifies the y coordinate of the lower right corner of
+     * the ROI.
+     */
+    private JTextField lowerRightY;
+
+    /**
+     * Used to display the current calibration pattern.
+     */
+    private ImagePanel calibImagePanel;
+   
+    /**
+     * Used to isplay an image from a CCD camera.  Also used in showing and
+     * specifying the ROI for the CCD.
+     */
+    private ImagePanel ccdImagePanel;
+
+    /**
+     * FileChooser widget used to select output folder location.
+     */
+    private JFileChooser folderChooser;
+
+    /**
+     * Textfield used to show/modify the output folder path.
+     */
+    private JTextField outPathEdit;
+
+    /**
+     * A button that triggers the folder chooser dialog to appear.
+     */
+    private JButton browseButton;
+
+    /**
+     * Constructs the Main class.  Sets up and displays the GUI.
+     */
     private Main() {        
         initComponents();        
     }
 
+    /**
+     * Builds and sets up the necessary panels.
+     */
     private void initComponents() {
         // Master panel.
         JPanel masterPanel = (JPanel)getContentPane();
         CellConstraints cc = new CellConstraints();
 
         // 2 columns.
-        String masterColSpec = "p:grow, 20dlu, p"; //3
+        String masterColSpec = "p:grow, 20dlu, fill:p"; //3
         String masterRowSpec = "fill:p:grow"; // 1
 
         FormLayout masterLayout = new FormLayout(masterColSpec, masterRowSpec);
@@ -76,7 +120,6 @@ public class Main extends JFrame {
 
         /* Setup the frame. */
         pack();
-        //setSize(800, 600);
         setLocationRelativeTo(null);
         setTitle("SLM LUT Diffraction Calibration");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -88,69 +131,72 @@ public class Main extends JFrame {
      * @return The left content panel.
      */
     private JPanel buildLeftPanel() {
-        String leftColSpec = "fill:p:grow"; // 1
-        String leftRowSpec   = "fill:p:grow, 4dlu, p"; //3
+        String leftColSpec = "p:grow"; // 1
+        String leftRowSpec   = "p, 7dlu, p"; //3
 
         CellConstraints cc = new CellConstraints();
         FormLayout leftLayout = new FormLayout(leftColSpec, leftRowSpec);
         PanelBuilder leftBuilder = new PanelBuilder(leftLayout);
 
+        /* Calibration Pattern Panel. */
+        String calibImageColSpecs = "400dlu";
+        String calibImageRowSpecs = "p, 4dlu, 250dlu";
+        FormLayout calibImageLayout =
+                new FormLayout(calibImageColSpecs, calibImageRowSpecs);
+        PanelBuilder calibImageBuilder = new PanelBuilder(calibImageLayout);
+
+        // Prepare contents.
+        calibImagePanel = new ImagePanel(false);
+
+        // Arrange contents.
+        int row = 1;
+        calibImageBuilder.addSeparator("Calibration Pattern",      cc.xy(1, row));
+        row += 2;
+        calibImageBuilder.add(calibImagePanel,                     cc.xy(1, row));
+
+        leftBuilder.add(calibImageBuilder.getPanel(),              cc.xy(1, 1));
+
+
         /* CCD Image Panel. */
-        String ccdColSpecs = "p:grow";
-        String ccdRowSpecs = "p, 4dlu, fill:p:grow";
+        String ccdColSpecs = "400dlu";
+        String ccdRowSpecs = "p, 4dlu, 250dlu";
         FormLayout ccdLayout = new FormLayout(ccdColSpecs, ccdRowSpecs);
         PanelBuilder ccdBuilder = new PanelBuilder(ccdLayout);
 
         // Prepare contents.
-        imagePanel = new ImagePanel();
-        imagePanel.setSize(500,500);
-
-        // Arrange contents.
-        int row = 1;
-        ccdBuilder.addSeparator("CCD Camera",               cc.xy(1, row));
-        row += 2;
-        ccdBuilder.add(imagePanel,                          cc.xy(1, row));
-
-        leftBuilder.add(ccdBuilder.getPanel(),              cc.xy(1, 1));
-
-
-        /* Status panel. */
-        String statColSpecs = "p, 4dlu, p"; // 3
-        String statRowSpecs = "p, 4dlu, p, 4dlu, p, 4dlu, p, 4dlu, p, 4dlu, p, 4dlu"; // 12
-        FormLayout statLayout = new FormLayout(statColSpecs, statRowSpecs);
-        PanelBuilder statBuilder = new PanelBuilder(statLayout);
-
-        // Prepare contents.
-        JLabel regionLabel = new JLabel("");
-        JLabel curValueLabel = new JLabel("");
-        JLabel refValueLabel = new JLabel("");
-        JLabel percentageLabel = new JLabel("");
-        JLabel roiIntensityLabel = new JLabel("");
-
+        ccdImagePanel = new ImagePanel(true);
+        ccdImagePanel.addObserver(this);
 
         // Arrange contents.
         row = 1;
-        statBuilder.addSeparator("Status",                 cc.xyw(1, row, 3));
+        ccdBuilder.addSeparator("CCD Camera",               cc.xy(1, row));
         row += 2;
-        statBuilder.addLabel("Region:"    ,                cc.xy(1, row));
-        statBuilder.add(regionLabel,                      cc.xy(3, row));
-        row += 2;
-        statBuilder.addLabel("Value:",                     cc.xy(1, row));
-        statBuilder.add(curValueLabel,                     cc.xy(3, row));
-        row += 2;
-        statBuilder.addLabel("Reference:",                 cc.xy(1, row));
-        statBuilder.add(refValueLabel,                     cc.xy(3, row));
-        row += 2;
-        statBuilder.addLabel("Percentage:",                cc.xy(1, row));
-        statBuilder.add(percentageLabel,                   cc.xy(3, row));
-        row += 2;
-        statBuilder.addLabel("ROI intensity:",             cc.xy(1, row));
-        statBuilder.add(roiIntensityLabel,                 cc.xy(3, row));
+        ccdBuilder.add(ccdImagePanel,                       cc.xy(1, row));
 
-        leftBuilder.add(statBuilder.getPanel(),            cc.xy(1, 3));
+        leftBuilder.add(ccdBuilder.getPanel(),              cc.xy(1, 3));
         
         return leftBuilder.getPanel();
     }
+
+    /**
+     * The update method listens for events from Observable objects.
+     * Currently that is the CCD Image Panel.
+     *
+     * @param o The observable object that registered the update.
+     * @param arg Input arguments, currently the actual class.
+     */
+    public void update(Observable o, Object arg) {
+        if (arg == null)
+            return;
+
+        Rectangle ROI = ((ImagePanel)arg).getROI();
+
+        upperLeftX.setText("" + ((int)ROI.getMinX()));
+        upperLeftY.setText("" + ((int)ROI.getMinY()));
+        lowerRightX.setText("" + ((int)ROI.getMaxX()));
+        lowerRightY.setText("" + ((int)ROI.getMaxY()));
+    }
+
 
     /**
      * Builds and returns the right panel.
@@ -158,15 +204,15 @@ public class Main extends JFrame {
      * @return The right content panel.
      */
     private JPanel buildRightPanel() {
-        String rightColSpec = "p"; // 1
-        String rightRowSpec   = "p, 4dlu, p, 4dlu, p, 4dlu, p"; //7
+        String rightColSpec = "fill:p"; // 1
+        String rightRowSpec   = "p, 7dlu, p, 7dlu, p, 7dlu, p, 7dlu, p"; // 9
 
         CellConstraints cc = new CellConstraints();
         FormLayout rightLayout = new FormLayout(rightColSpec, rightRowSpec);
         PanelBuilder rightBuilder = new PanelBuilder(rightLayout);
 
         /* ROI Panel. */
-        String roiColSpecs = "p, 4dlu, p, 4dlu, 20dlu, 4dlu, p, 4dlu, 20dlu"; // 9
+        String roiColSpecs = "p, 4dlu, p, 4dlu, 20dlu, 4dlu, p, 4dlu, 20dlu, 4dlu:grow"; // 10
         String roiRowSpecs = "p, 4dlu, p, 4dlu, p, 4dlu, p"; // 7
         FormLayout roiLayout = new FormLayout(roiColSpecs, roiRowSpecs);
         PanelBuilder roiBuilder = new PanelBuilder(roiLayout);
@@ -183,14 +229,14 @@ public class Main extends JFrame {
                 Integer y1 = new Integer(upperLeftY.getText());
                 Integer x2 = new Integer(lowerRightX.getText());
                 Integer y2 = new Integer(lowerRightY.getText());
-                imagePanel.setRectangle(x1, y1, x2, y2);
-                imagePanel.repaint();
+                ccdImagePanel.setRectangle(x1, y1, x2, y2);
+                ccdImagePanel.repaint();
             }
         });
 
         // Arrange items.
         int row = 1;
-        roiBuilder.addSeparator("Define ROI",               cc.xyw(1, row, 9));
+        roiBuilder.addSeparator("Define ROI",               cc.xyw(1, row, 10));
         row += 2;
         roiBuilder.addLabel("Upper left:",                  cc.xy(1, row));
         roiBuilder.addLabel("X:",                           cc.xy(3, row));
@@ -199,7 +245,7 @@ public class Main extends JFrame {
         roiBuilder.add(upperLeftY,                          cc.xy(9, row));
 
         row += 2;
-        roiBuilder.addLabel("Lower right",                  cc.xy(1, row));
+        roiBuilder.addLabel("Lower right:",                  cc.xy(1, row));
         roiBuilder.addLabel("X:",                           cc.xy(3, row));
         roiBuilder.add(lowerRightX,                         cc.xy(5, row));
         roiBuilder.addLabel("Y:",                           cc.xy(7, row));
@@ -211,30 +257,37 @@ public class Main extends JFrame {
         rightBuilder.add(roiBuilder.getPanel(),             cc.xy(1, 1));
 
         /* Calibration Settings Panel. */
-        String calibColSpecs = "p, 4dlu, 30dlu"; // 3
-        String calibRowSpecs = "p, 4dlu, p, 4dlu, p"; // 5
+        String calibColSpecs = "p, 4dlu, 30dlu, 4dlu:grow"; // 4
+        String calibRowSpecs = "p, 4dlu, p, 4dlu, p, 4dlu, p"; // 7
         FormLayout calibLayout = new FormLayout(calibColSpecs, calibRowSpecs);
         PanelBuilder calibBuilder = new PanelBuilder(calibLayout);
 
         // Prepare contents.
         JTextField gratingsEdit = new JTextField("");
         JTextField regionsEdit = new JTextField("");
+        JButton calSetButton = new JButton("Set");
+        calSetButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Set");
+            }
+        });
 
         // Arrange contents.
         row = 1;
-        calibBuilder.addSeparator("Calibration Settings",   cc.xyw(1, row, 3));
+        calibBuilder.addSeparator("Calibration Settings",   cc.xyw(1, row, 4));
         row += 2;
         calibBuilder.addLabel("# Gratings:",                cc.xy(1, row));
         calibBuilder.add(gratingsEdit,                      cc.xy(3, row));
         row += 2;
         calibBuilder.addLabel("# Regions:",                 cc.xy(1, row));
         calibBuilder.add(regionsEdit,                       cc.xy(3, row));
+        row += 2;
+        calibBuilder.add(calSetButton,                       cc.xy(1, row));
 
-        rightBuilder.add(calibBuilder.getPanel(),            cc.xy(1, 3));
-
+        rightBuilder.add(calibBuilder.getPanel(),           cc.xy(1, 3));
 
         /* Output Panel. */
-        String outpColSpecs = "p, 4dlu, 60dlu, 4dlu, p"; // 5
+        String outpColSpecs = "p, 4dlu, 120dlu, 4dlu, p, 4dlu:grow"; // 6
         String outpRowSpecs = "p, 4dlu, p"; // 3
         FormLayout outpLayout = new FormLayout(outpColSpecs, outpRowSpecs);
         PanelBuilder outpBuilder = new PanelBuilder(outpLayout);
@@ -256,7 +309,7 @@ public class Main extends JFrame {
 
         // Arrange items.
         row = 1;
-        outpBuilder.addSeparator("Output options",          cc.xyw(1, row, 5));
+        outpBuilder.addSeparator("Output options",          cc.xyw(1, row, 6));
         row += 2;
         outpBuilder.addLabel("Output Folder:",              cc.xy(1, row));
         outpBuilder.add(outPathEdit,                        cc.xy(3, row));
@@ -264,29 +317,97 @@ public class Main extends JFrame {
 
         rightBuilder.add(outpBuilder.getPanel(),            cc.xy(1, 5));
 
-        /* Operations Panel. */
-        String opColSpecs = "p, 4dlu, p"; // 3
+        /* Measurement Operations Panel. */
+        String opColSpecs = "p, 4dlu, p, 4dlu:grow"; // 4
         String opRowSpecs = "p, 4dlu, p"; // 3
         FormLayout opLayout = new FormLayout(opColSpecs, opRowSpecs);
         PanelBuilder opBuilder = new PanelBuilder(opLayout);
 
         // Prepare items.
         JButton startButton = new JButton("Start");
+        startButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Start button pressed");
+                runCamera();
+            }
+        });
         JButton cancelButton = new JButton("Cancel");
+        cancelButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Cancel");
+            }
+        });
 
         // Arrange items.
         row = 1;
-        opBuilder.addSeparator("Operations",                cc.xyw(1, row, 3));
+        opBuilder.addSeparator("Operations",                cc.xyw(1, row, 4));
         row += 2;
         opBuilder.add(startButton,                          cc.xy(1, row));
         opBuilder.add(cancelButton,                         cc.xy(3, row));
 
         rightBuilder.add(opBuilder.getPanel(),              cc.xy(1, 7));
 
+        /* Status panel. */
+        String statColSpecs = "p, 4dlu, p, 4dlu:grow"; // 4
+        String statRowSpecs = "p, 4dlu, p, 4dlu, p, 4dlu, p, 4dlu, p, 4dlu, p, 4dlu"; // 12
+        FormLayout statLayout = new FormLayout(statColSpecs, statRowSpecs);
+        PanelBuilder statBuilder = new PanelBuilder(statLayout);
+
+        // Prepare contents.
+        JLabel regionLabel = new JLabel("");
+        JLabel curValueLabel = new JLabel("");
+        JLabel refValueLabel = new JLabel("");
+        JLabel percentageLabel = new JLabel("");
+        JLabel roiIntensityLabel = new JLabel("");
+
+        // Arrange contents.
+        row = 1;
+        statBuilder.addSeparator("Status",                 cc.xyw(1, row, 4));
+        row += 2;
+        statBuilder.addLabel("Region:"    ,                cc.xy(1, row));
+        statBuilder.add(regionLabel,                       cc.xy(3, row));
+        row += 2;
+        statBuilder.addLabel("Value:",                     cc.xy(1, row));
+        statBuilder.add(curValueLabel,                     cc.xy(3, row));
+        row += 2;
+        statBuilder.addLabel("Reference:",                 cc.xy(1, row));
+        statBuilder.add(refValueLabel,                     cc.xy(3, row));
+        row += 2;
+        statBuilder.addLabel("Percentage:",                cc.xy(1, row));
+        statBuilder.add(percentageLabel,                   cc.xy(3, row));
+        row += 2;
+        statBuilder.addLabel("ROI intensity:",             cc.xy(1, row));
+        statBuilder.add(roiIntensityLabel,                 cc.xy(3, row));
+
+        rightBuilder.add(statBuilder.getPanel(),            cc.xy(1, 9));
+
         return rightBuilder.getPanel();
+    }
+
+    /**
+     * Runs the camera (for test purposes).
+     */
+    private void runCamera() {
+        CCDCamera ccdCamera = new CCDCamera();
+
+        System.out.println("testMe(): " + ccdCamera.testMe());
+
+        if (ccdCamera.initialize()) {
+            System.out.println("Successfully initialized the CCD Camera");
+
+        } else {
+            System.out.println("Failed to initialize the CCD Camera");
+        }
+
+        System.out.println("Note: " + ccdCamera.getNote());
+        
+        int frameLen = ccdCamera.captureFrame();
+        System.out.println("frameLength: " + frameLen);
     }
     
     /**
+     * Starts the application.
+     *
      * @param args The command line arguments.
      */
     public static void main(String[] args) {
@@ -303,27 +424,5 @@ public class Main extends JFrame {
         }
 
         new Main().setVisible(true);
-
-        // TODO code application logic here
-
-        /*
-        CCDCamera ccdCamera = new CCDCamera();
-
-        System.out.println("testMe(): " + ccdCamera.testMe());
-
-        if (ccdCamera.initialize()) {
-            System.out.println("Successfully initialized the CCD Camera");
-
-        } else {
-            System.out.println("Failed to initialize the CCD Camera");
-        }
-        
-        System.out.println("Note: " + ccdCamera.getNote());
-
-      
-        int frameLen = ccdCamera.captureFrame();
-        System.out.println("frameLength: " + frameLen);  
-         * 
-         */
     }
 }
